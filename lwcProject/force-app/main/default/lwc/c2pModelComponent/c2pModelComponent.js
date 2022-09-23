dc0a8f70f4b6c4107425bc1a243989c5b000cd35
Piyumi047 <dulanjaligedarawtta@gmail.com>
2022-09-23 22:29:38 +0530

    lwc nortification

--- lwcProject/force-app/main/default/lwc/c2pModelComponent/c2pModelComponent.js
+++ lwcProject/force-app/main/default/lwc/c2pModelComponent/c2pModelComponent.js
@@ -3,7 +3,7 @@ import { LightningElement } from 'lwc';
 export default class C2pModelComponent extends LightningElement {
     closeHandler(){
         const myevent= new CustomEvent('close',{
-            bubbles:true,
+            //bubbles:true,
             detail:"Model closed successfully!!"
         })
         this.dispatchEvent(myevent)
--- lwcProject/force-app/main/default/lwc/customLabelDemo/customLabelDemo.js
+++ lwcProject/force-app/main/default/lwc/customLabelDemo/customLabelDemo.js
@@ -2,8 +2,8 @@ import { LightningElement } from 'lwc';
 import DESCRIPTION_ONE from '@salesforce/label/c.description'
 import DESCRIPTION_TWO from '@salesforce/label/c.descriptionTwo'
 export default class CustomLabelsDemo extends LightningElement {
-    LABELS = { 
-        descriptionOne:DESCRIPTION_ONE,
-        descriptionTwo:DESCRIPTION_TWO
+    LABELS={ 
+        descriptionOne:DESCRIPTION_ONE
+       // descriptionTwo:DESCRIPTION_TWO
     }
 }
